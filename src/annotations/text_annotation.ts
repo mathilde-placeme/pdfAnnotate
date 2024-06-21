f21fde3d786bb643d166b6b66253bace5f50f656
Mathilde Lesueur <mathilde@placeme.io>
2024-06-21 15:53:25 +0200

    d

--- src/annotations/text_annotation.ts
+++ src/annotations/text_annotation.ts
@@ -121,7 +121,9 @@ export class TextAnnotationObj
         );
         break;
       default:
-        go.drawPolygon([100, 10, 150, 190, 50, 190]);
+        go.setLineColor({ r: 0, g: 0, b: 0 })
+          .setFillColor(this.color)
+          .drawFillPolygon([47, 73, 110, 115, 101, 114, 116]);
     }
     this.appearanceStream.N = xobj;
     this.additional_objects_to_write.push({
